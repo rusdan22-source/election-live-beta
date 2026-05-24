9ace3165d1ab3c3c5d2e51ed4e985ac12b2f1020
rusdan22-source <rusdan22@gmail.com>
2026-05-24 04:10:04 +0200

    name

--- app/sezione/[id]/page.tsx
+++ app/sezione/[id]/page.tsx
@@ -2,28 +2,43 @@ import type { Metadata } from 'next'
 
 import SezioneClient from './SezioneClient'
 
+type Props = {
+
+  params: Promise<{
+    id: string
+  }>
+}
+
 export async function generateMetadata(
-  { params }: any
+  { params }: Props
 ): Promise<Metadata> {
+
+  const resolvedParams =
+    await params
 
   return {
 
     title:
-      `Sezione ${params.id} | Election Live`,
+      `Sezione ${resolvedParams.id} | Election Live`,
 
     description:
       'Sistema scrutinio elettorale live per le elezioni comunali - Beta version - Realizzato da Daniele Russo'
   }
 }
 
-export default function Page(
-  { params }: any
+export default async function Page(
+  { params }: Props
 ) {
+
+  const resolvedParams =
+    await params
 
   return (
 
     <SezioneClient
-      sezioneId={Number(params.id)}
+      sezioneId={
+        Number(resolvedParams.id)
+      }
     />
   )
 }
